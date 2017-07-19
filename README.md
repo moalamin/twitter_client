5e589dc391199ca84d5f663047c76dd2dca022da
Mo Amin <m.alamin92@gmail.com>
2017-07-19 00:46:38 -0400

    first commit, twitter client written by mo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Twitter Client
+------
+
+This a twitter client I wrote to help me gain followers. It is for personal use only. The main functionalities are looking for tweets that have a certain hashtag set by the user. Then, filter the names of the user of those tweets. Follow every user. Then, there is a function that compares my following list to my followers and unfollows those users that do not follow me as well.
+
+The idea is to use this to find users that have tweeted about my interests and follow them. If they follow me back, we will have something in common. It will help me increase twitter followers.
---
+++ twit_client.rb
@@ -0,0 +1,73 @@
+
+# TwitterClient Class
+class TwitterClient
+  require 'twitter'
+  require 'pp'
+  # Initialize with twitter API access information.
+  # This should be the string of the name of the enviroment variable
+  def initialize(consumer_key, consumer_secret, access_token, access_token_secret)
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV[consumer_key]
+      config.consumer_secret     = ENV[consumer_secret]
+      config.access_token        = ENV[access_token]
+      config.access_token_secret = ENV[access_token_secret]
+    end
+  end
+
+  def unfollow_unfollowers
+    friends = get_users_friend_ids(@client)
+    followers = get_users_follower_ids(@client)
+
+    friends.each do |friend|
+      @client.unfollow(friend) unless followers.include?(friend)
+    end
+  end
+
+  def follow_by_query(query)
+    users = search_tweets(query)
+    follow_hashtag_users(users)
+  end
+
+  private
+
+  def users_follower_ids
+    all_followers = []
+    followers_cursor = @client.follower_ids
+    follower_ids_array = followers_cursor.attrs[:ids]
+    all_followers.concat(follower_ids_array)
+    all_followers
+  end
+
+  def users_friend_ids
+    all_friends = []
+    friends_cursor = @client.friend_ids
+    friend_ids_array = friends_cursor.attrs[:ids]
+    all_friends.concat(friend_ids_array)
+    all_friends
+  end
+
+  def search_tweets(search_query)
+    tweets = @client.search(search_query, result_type: 'recent', count: 100)
+    tweets.attrs[:statuses].map { |status| status[:user][:id] }
+  end
+
+  def follow_hashtag_users(users)
+    users.each_with_index do |user, index|
+      begin
+        puts "AT INDEX: #{index}"
+        @client.follow(user)
+      rescue
+        puts "RATE LIMIT REACHED AT: ---#{Time.now}---"
+        sleep(960)
+        puts "RATE LIMITED -- retrying: #{index}"
+        retry
+      end
+    end
+  end
+end
+
+client = TwitterClient.new('TWITTER_CONSUMER_KEY',
+                           'TWITTER_CONSUMER_SECRET',
+                           'TWITTER_ACCESS_TOKEN',
+                           'TWITTER_ACCESS_TOKEN_SECRET')
+client.follow_by_query('@rails')
